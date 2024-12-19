b553142d8e6535245b164bbd42548c9c23f09ad4
jm <tumbagajohn@gmail.com>
2024-12-19 15:08:40 +0800

    Generated link using unique link instead of finalURL

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,46 @@ const qrCode = new QRCodeStyling({
   },
 });
 
+const generateUniqueLink = (finalURL, campaignId) => {
+  console.log();
+  return `${import.meta.env.VITE_REACT_APP_BASE_URL}/?finalURL=${finalURL}&campaignID=${campaignId}`;
+};
 export default function App() {
-  const [url, setUrl] = useState("https://asianlink.ai");
+  const [url, setUrl] = useState("https://asianlink.ai/redirect/download");
   const [fileExt, setFileExt] = useState("png");
+  const [shape, setShape] = useState("square");
+  const [dotType, setDotType] = useState("square");
+  const [dotColor, setDotColor] = useState("#000000");
+  const [cornerDotType, setCornerDotType] = useState("square");
+  const [cornerDotColor, setCornerDotColor] = useState("#000000");
+  const [campaignId, setCampaignId] = useState(1);
+  const [finalURL, setFinalURL] = useState(
+    "https://asianlink.ai/redirect/download",
+  );
+
   const ref = useRef(null);
 
   useEffect(() => {
     qrCode.append(ref.current);
   }, []);
-
+  useEffect(() => {
+    setUrl(generateUniqueLink(finalURL, campaignId));
+  }, [finalURL, campaignId]);
   useEffect(() => {
     qrCode.update({
       data: url,
+      shape: shape,
+      dotsOptions: {
+        type: dotType,
+        color: dotColor,
+        roundSize: true,
+      },
+      cornersDotOptions: {
+        type: cornerDotType,
+        color: cornerDotColor,
+      },
     });
-  }, [url]);
+  }, [url, shape, dotType, dotColor, cornerDotType, cornerDotColor]);
 
   const onUrlChange = (event) => {
     event.preventDefault();
@@ -65,16 +91,62 @@ export default function App() {
     setFileExt(event.target.value);
   };
 
+  const onShapeChange = (event) => {
+    setShape(event.target.value);
+  };
+
+  const onDotTypeChange = (event) => {
+    setDotType(event.target.value);
+  };
+
+  const onDotColorChange = (event) => {
+    setDotColor(event.target.value);
+  };
+
+  const onCornerDotTypeChange = (event) => {
+    setCornerDotType(event.target.value);
+  };
+
+  const onCornerDotColorChange = (event) => {
+    setCornerDotColor(event.target.value);
+  };
+
   const onDownloadClick = () => {
     qrCode.download({
       extension: fileExt,
     });
   };
 
+  const onCampaignIdChange = (event) => {
+    setCampaignId(event.target.value);
+  };
+
+  const onFinalURLChange = (event) => {
+    setFinalURL(event.target.value);
+  };
+
   return (
-    <div className="App flex flex-col bg-white">
+    <div className="App flex flex-col">
+      <div ref={ref} />
       <div className="mt-[20px] flex justify-between w-full">
-        <input value={url} onChange={onUrlChange} className="grow-1 mr-20" />
+        <div className="flex flex-col">
+          <div className="flex">
+            <label>Final URL: </label>
+            <input
+              value={finalURL}
+              onChange={onFinalURLChange}
+              className="grow-1 mr-20"
+            />
+          </div>
+          <div className="flex">
+            <label>Campaign: </label>
+            <input
+              value={campaignId}
+              onChange={onCampaignIdChange}
+              className="grow-1 mr-20"
+            />
+          </div>
+        </div>
         <select onChange={onExtensionChange} value={fileExt}>
           <option value="png">PNG</option>
           <option value="jpeg">JPEG</option>
@@ -82,9 +154,43 @@ export default function App() {
         </select>
         <button onClick={onDownloadClick}>Download</button>
       </div>
-      <div ref={ref} />
-      <div className="bg-blue-500 text-white p-4">
-        This should have a blue background, white text, and padding.
+
+      <div className="mt-4">
+        <label>Shape: </label>
+        <select value={shape} onChange={onShapeChange}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+      </div>
+
+      <div className="mt-4">
+        <label>Dot Type: </label>
+        <select value={dotType} onChange={onDotTypeChange}>
+          <option value="square">Square</option>
+          <option value="dot">Dot</option>
+        </select>
+      </div>
+
+      <div className="mt-4">
+        <label>Dot Color: </label>
+        <input type="color" value={dotColor} onChange={onDotColorChange} />
+      </div>
+
+      <div className="mt-4">
+        <label>Corner Dot Type: </label>
+        <select value={cornerDotType} onChange={onCornerDotTypeChange}>
+          <option value="square">Square</option>
+          <option value="dot">Dot</option>
+        </select>
+      </div>
+
+      <div className="mt-4">
+        <label>Corner Dot Color: </label>
+        <input
+          type="color"
+          value={cornerDotColor}
+          onChange={onCornerDotColorChange}
+        />
       </div>
     </div>
   );
